1d5060c4a7cf2c15c8ad143377d25d3078e7c8b0
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2021-09-10 16:50:53 -0400

    update notebooks, plotting script

--- code/plotter.py
+++ code/plotter.py
@@ -1,13 +1,19 @@
+import h5py
 import numpy as np
+import pickle
 from matplotlib import pyplot as plt
 
+import getdist
+from getdist import plots, MCSamples
+
+import utils
 from utils import *
 
 
 def compare_accuracy(statistic, train_tags, labels, colors):
     nrows = 2
     fig, axarr = plt.subplots(nrows, 1, figsize=(10,10), gridspec_kw={'height_ratios': [1]*nrows})
-    plt.subplots_adjust(hspace=0.15)
+    plt.subplots_adjust(hspace=0.25)
 
     fn_test = '../tables/id_pairs_test.txt'
     id_pairs_test = np.loadtxt(fn_test, delimiter=',', dtype='int')
@@ -39,9 +45,19 @@ def compare_accuracy(statistic, train_tags, labels, colors):
         err_frac_std = np.std(errs_frac, axis=0)
         axarr[0].plot(r_vals, err_frac_std, label=labels[j], color=colors[j])
 
+        err_frac_p16 = np.percentile(errs_frac, 16, axis=0)
+        err_frac_p84 = np.percentile(errs_frac, 84, axis=0)
+        axarr[1].plot(r_vals, err_frac_p16, label=labels[j], color=colors[j])
+        axarr[1].plot(r_vals, err_frac_p84, label=labels[j], color=colors[j])
+
     axarr[0].set_xscale(scale_dict[statistic][0])
     axarr[0].set_xlabel(r_labels[statistic])
-    axarr[0].set_ylabel('error')
+    axarr[0].set_ylabel('error (stdev)')
+
+    axarr[1].set_xscale(scale_dict[statistic][0])
+    axarr[1].set_xlabel(r_labels[statistic])
+    axarr[1].set_ylabel('error (16-84 percentile)')
+    axarr[1].axhline(0, color='k', lw=0.5)
 
     axarr[0].legend()
 
@@ -218,3 +234,66 @@ def compare_emulators(statistic, testtags, acctags, errtag, savetags, labels=Non
         ax[1].set_ylabel("fractional error (16/84 percentile)")
 
     ax[0].legend()
+
+
+def plot_contours(chaintags, legend_labels=None, params_toplot=None, colors=None,
+                  legend_loc='upper center', legend_fontsize=20,
+                  vertical_markers=None, vertical_marker_color='grey'):
+    # Make dict of bounds for plot ranges
+    bounds = utils.get_bounds()
+    sample_arr = []
+    for i, chaintag in enumerate(chaintags):
+
+        chain_fn = f'../chains/param_files/chain_params_{chaintag}.h5'
+        fw = h5py.File(chain_fn, 'r')
+        param_names = fw.attrs['param_names_vary']
+        if vertical_markers is None:
+            vertical_markers_toplot = fw.attrs['true_values']
+        else:
+            vertical_markers_toplot = vertical_markers
+        fw.close()
+
+        chain_results_dir = '/home/users/ksf293/aemulator/chains/results'
+        chain_results_fn = f'{chain_results_dir}/results_{chaintag}.pkl'
+        with open(chain_results_fn, 'rb') as pf:
+            res = pickle.load(pf)
+            samples = res['samples']
+            lnweight = np.array(res['logwt'])
+            lnevidence = np.array(res['logz'])
+
+        # Select only samples for desired parameters
+        if params_toplot is None:
+            params_toplot = param_names
+        else:
+            idxs = []
+            for pm in params_toplot:
+                idxs.append(np.where(param_names == pm))
+            idxs = np.array(idxs).flatten()
+            samples = samples[:,idxs]
+            # Note: weight & evidence same for all params, shape (n_samps,), so don't need to slice
+            vertical_markers_toplot = vertical_markers_toplot[idxs]
+
+        # param_labels is in utils
+        labels = [param_labels[pn] for pn in params_toplot]
+        ranges = [bounds[pn] for pn in params_toplot]
+
+        #[-1] bc just care about final evidence value
+        weights = np.exp(lnweight - lnevidence[-1])
+        weights = weights.flatten()
+
+        samps = getdist.MCSamples(names=params_toplot, labels=labels)
+        samps.setSamples(samples, weights=weights)
+        sample_arr.append(samps)
+
+    marker_args = {'color': vertical_marker_color}
+
+    g = getdist.plots.get_subplot_plotter()
+    g.settings.alpha_filled_add=0.4
+    g.settings.figure_legend_frame = False
+    g.settings.legend_fontsize = legend_fontsize
+    g.settings.axis_marker_lw = 1.0
+    g.settings.axis_marker_color = 'dimgrey'
+    g.triangle_plot(sample_arr, filled=True, contour_colors=colors, names=params_toplot,
+                   legend_labels=legend_labels, markers=vertical_markers_toplot, title_limit=1, legend_loc=legend_loc,
+                    marker_args=marker_args, axis_marker_color='red')
+    return g
